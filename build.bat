dc1d49003a49c1952fd7a81f608748d6cf547a3f
unknown <hsmgj2010@gmail.com>
2022-02-26 02:45:27 +0800

    change to 32 bit target

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target x86_64-pc-windows-msvc --release
-copy target\x86_64-pc-windows-msvc\release\pvz_widescreen.exe dist\pvz_widescreen.exe
+cargo +nightly build -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort --target i686-pc-windows-msvc --release
+copy target\i686-pc-windows-msvc\release\pvz_widescreen.exe dist\pvz_widescreen.exe
